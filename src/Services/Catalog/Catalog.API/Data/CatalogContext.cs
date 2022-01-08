2929108afff243720da5b7089629927aa42e478e
carlosoproprio <carlos.o.proprio@gmail.com>
2022-01-08 17:44:00 +0000

    cleanup

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -8,11 +8,12 @@ namespace Catalog.API.Data
     {
         public CatalogContext(IConfiguration configuration)
         {
-            var client = new MongoClient(configuration.GetValue<string>("MongoDatabaseSettings:ConnectionString"));
-            var database = client.GetDatabase(configuration.GetValue<string>("MongoDatabaseSettings:DatabaseName"));
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
-            Products = database.GetCollection<Product>(configuration.GetValue<string>("MongoDatabaseSettings:CollectionName"));
-            //CatalogContextSeed
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
         }
 
         public IMongoCollection<Product> Products { get; }
