900b3fdf7374b5286fad88baaeef135a9e8d352b
carlosoproprio <carlos.o.proprio@gmail.com>
2022-01-08 16:36:06 +0000

    Added DataLayer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("MongoDatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("MongoDatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("MongoDatabaseSettings:CollectionName"));
+            CatalogContextSeed
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+
+}
