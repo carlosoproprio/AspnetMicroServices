2844e197337153c781d3bf8eea397a928557c158
carlosoproprio <carlos.o.proprio@gmail.com>
2022-01-09 00:49:37 +0000

    Added BasketAPI solution folder

--- src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -10,6 +10,7 @@ namespace Catalog.API.Data
         public static void SeedData(IMongoCollection<Product> productCollection)
         {
             bool existProduct = productCollection.Find(p => true).Any();
+
             if(!existProduct)
             {
                 productCollection.InsertManyAsync(GetPreconfigureProducts());
